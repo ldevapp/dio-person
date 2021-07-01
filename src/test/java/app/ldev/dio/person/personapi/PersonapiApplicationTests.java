ae069be442e65d850bc58a388b9a7ccdb13a51b8
Leandro <leandro_csilva@ymail.com>
2021-07-01 17:07:44 -0300

    Commit inicial com estrutura básica do projeto

---
+++ src/test/java/app/ldev/dio/person/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ldev.dio.person.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
